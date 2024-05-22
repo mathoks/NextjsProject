85ddab2d3b11c2b3c92e7f30e48cec91cf1160df
mathoks <mathoks2012@gmail.com>
2024-05-22 22:21:18 +0100

    gghopkklcc

--- auth.js
+++ auth.js
@@ -4,19 +4,20 @@ import { PrismaAdapter } from '@auth/prisma-adapter'
 import prisma from '@/models/model'
 
 
+
 export const {handlers, auth, signIn, signOut} = NextAuth(
     {
+        basePath:"/api/auth",
         adapter: PrismaAdapter(prisma),
         providers:[Google],
         trustHost: true,
-        basePath:'/api/auth',
-        secret: process.env.AUTH_SECRET,
+        
         callbacks: {
-            session({session, user}){
+           async session({session, user}){
                 session.user.role = user.role
                 return session
             },
-            signIn: ({user, email, profile})=>{
+           async signIn({user, email, profile}){
                 console.log(user)
             }
         }
