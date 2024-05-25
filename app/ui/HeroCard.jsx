627dfa1499bb629190f9c12230e1295e6ebd0977
mathoks <mathoks2012@gmail.com>
2024-05-25 20:26:51 +0100

    swee

--- app/ui/HeroCard.jsx
+++ app/ui/HeroCard.jsx
@@ -19,24 +19,26 @@ const HeroCard = () => {
   ];
 
   return (
-    <div className="bg-gradient-to-t relative from-slate-300 via-slate-500 to-slate-600 flex flex-col w-full h-auto">
+    <div className="bg-gradient-to-t relative from-slate-100 via-slate-500 to-slate-600 flex flex-col w-full h-auto">
       <div>
         <Carousel slides={slides} autoSlide={true} />
       </div>
-      <div className="flex gap-2 overflow-x-scroll absolute inset-y-[14rem] h-[11rem] z-40 w-full pl-2 pr-2">
+      <div className="flex gap-2 overflow-x-scroll absolute inset-y-[14rem] h-[10rem] z-40 w-full pl-2 pr-2">
         {[Logo, Logo2, Logo3, Logo1, Logo2, Logo3, Logo].map((img, id) => (
-          <div key={id} className=" bg-white flex flex-col space-y-0 rounded-md pb-2  shadow-md">
+          <div key={id} className=" bg-white flex flex-col space-y-0 rounded-md shadow-2xl">
             <div className="text-left p-4 text-nowrap rounded-r-md rounded-md">
               <h3>Amazing Gifts</h3>
             </div>
+           
             <Image
               src={img}
-              height={200}
-              width={200}
+              height={300}
+              width={300}
               alt="pics"
               key={id}
-              className="shrink-0 object-cover h-40"
+              className="shrink-0 h-[6.5rem] rounded-b-md"
             />
+            
           </div>
         ))}
       </div>
