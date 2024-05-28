6df848f37ab720d86b863d05a0726b8451396b09
mathoks <mathoks2012@gmail.com>
2024-05-28 12:16:22 +0100

    mbdfasf

--- app/actions/users/getUsers.js
+++ app/actions/users/getUsers.js
@@ -5,7 +5,7 @@
  */
 export async function getUsers(){
     try {
-    const res = await fetch("http://localhost:3000/api/users " || "https://nextjs-project-if9d-git-master-mathoks-projects.vercel.app/api/users")
+    const res = await fetch("https://nextjs-project-if9d-git-master-mathoks-projects.vercel.app/api/users")
     
     const users = await res.json()
     return users.data
