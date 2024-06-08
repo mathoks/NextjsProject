cc275038de44a93e02bf91921190429623b37b33
mathoks <mathoks2012@gmail.com>
2024-06-08 10:25:53 +0100

    fixconn36

--- app/ui/Nav.jsx
+++ app/ui/Nav.jsx
@@ -42,7 +42,7 @@ console.log(myNav.getState())
 
   return (
     nav && path === '/' ? (
-    <nav aria-label="main" className=" flex flex-col space-y-0  pb-[1px] fixed w-full z-50  shadow-lg" >
+    <nav aria-label="main" className=" flex flex-col fixed w-full z-50  shadow-lg h-[158px]" >
     <div className="sm: flex flex-col space-y-4  bg-[#4f08ed] md:flex items-center justify-center p-4">
     <section>
       <Link href={"#"} className="flex  gap-4 items-center">
