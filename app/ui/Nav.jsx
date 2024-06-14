fb44586fa42b9906ce79afdd32d168d7454daa3b
mathoks <mathoks2012@gmail.com>
2024-06-14 10:16:12 +0100

    fixconn63

--- app/ui/Nav.jsx
+++ app/ui/Nav.jsx
@@ -73,7 +73,7 @@ useEffect(()=>{
       </Link>
       </section>
       <section>
-      <div className="flex w-[90%] justify-center">
+      <div className="flex pl-4 pr-4 justify-center ">
         <div className="border flex items-center justify-between bg-white p-2 rounded-md ">
         <SearchOutlined className=" text-[#6A0DAD]" fontSize="medium" sx={{ zIndex: 80}}/>
         <input placeholder="find here" className="placeholder:text-center text-zinc-950  pl-4 pr-4 focus:outline-none w-[98%]"/>
