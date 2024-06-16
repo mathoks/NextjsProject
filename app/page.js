80ac223ebb0e7cc1e0d18da70df455030737d99b
mathoks <mathoks2012@gmail.com>
2024-06-16 18:38:43 +0100

    fixconn75

--- app/page.js
+++ app/page.js
@@ -95,7 +95,7 @@ export default async function Home() {
            <section>
            
           <Suspense fallback={<Loading/>}>
-           <Products info = {usersList ? JSON.parse(usersList): []}/>
+           <Products info = {typeof usersList !== "undefined" ? JSON.parse(usersList): []}/>
            </Suspense>
            </section>
           
