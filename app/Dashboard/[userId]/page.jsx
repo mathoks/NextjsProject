d1271adf997e03d19e52610d0a0c03f6cbceab67
mathoks <mathoks2012@gmail.com>
2024-06-20 15:40:21 +0100

    fixconn98

--- app/Dashboard/[userId]/page.jsx
+++ app/Dashboard/[userId]/page.jsx
@@ -1,12 +1,12 @@
 
-// import ProgressBar from '@/app/ui/utilComp/ProgressBar'
+import ProgressBar from '@/app/ui/utilComp/ProgressBar'
 import { auth } from '@/auth'
 import { Settings } from '@mui/icons-material'
 import { Avatar} from '@mui/material'
 import dynamic from 'next/dynamic'
 import React from 'react'
 
-const ProgressBar = dynamic(()=>import('../../ui/utilComp/ProgressBar'), {ssr: false})
+// const ProgressBar = dynamic(()=>import('../../ui/utilComp/ProgressBar'), {ssr: false})
 const page = async() => {
     const session = await auth()
 const {user: {name, image}} = session
@@ -24,7 +24,7 @@ const {user: {name, image}} = session
             <p className='text-sm'>Lamb industries limited</p>
             </span>
         </div>
-        <div className='bg-white flex flex-col rounded-t-md h-20 p-4 text-black space-y-3 z-40 shadow'>
+        <div className='bg-white flex flex-col rounded-t-md h-24 p-4 text-black space-y-3 z-40 shadow'>
             <span className='flex justify-between items-center'><p>Complete your profile</p><button className='py-1 px-4 bg-[#6A0DAD] text-white rounded-full'>Go</button></span>
             <span>
                 <ProgressBar value={70}/>
