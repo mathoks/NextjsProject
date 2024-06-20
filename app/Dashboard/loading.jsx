4ae50949ab7b95b20056ca3bceb354e091c71bf4
mathoks <mathoks2012@gmail.com>
2024-06-20 15:20:13 +0100

    fixconn97

--- app/Dashboard/loading.jsx
+++ app/Dashboard/loading.jsx
@@ -2,7 +2,7 @@ import { CircularProgress } from '@mui/material'
 import React from 'react'
 
 const Dloading = () => {
-  return (<div className='h-screen mx-auto'>
+  return (<div className='h-screen flex items-center justify-center'>
   <CircularProgress/>
   </div>
     
