7c44629d5ccf5ce43e51243668c2ed708857ff0f
mathoks <mathoks2012@gmail.com>
2024-06-23 10:45:48 +0100

    fixsettings5

--- app/Dashboard/[userId]/settings/page.jsx
+++ app/Dashboard/[userId]/settings/page.jsx
@@ -3,24 +3,24 @@ import React from 'react'
 
 const page = () => {
   return (
-    <div className='b bg-gray-300 w-screen text-slate-950'>
-        <section className='flex justify-start items-center bg-white w-screen p-4'>
+    <div className='b bg-gray-300 w-screen text-slate-950 flex flex-col space-y-4'>
+        <section className='flex justify-between items-center bg-white w-screen p-4'>
             <ArrowBackOutlined fontSize='medium'/>
             <span>Settings</span>
             <span></span>
         </section>
         <section className='bg-white space-y-2 p-4'>
-        <span className='flex'>
+        <span className='flex justify-between'>
         <span>Language</span>
-        <span className='flex text-gray-300 space-x-2 items-center'>
+        <span className='flex text-gray-400 space-x-2 items-center text-sm'>
         <span>English</span>
         <ArrowForwardOutlined/>
         </span>
         </span>
-        <span className='w-screen h-1 bg-gray-300'/>
-        <span className='flex'>
+        <span className='w-screen h-1 bg-gray-300'></span>
+        <span className='flex justify-between'>
         <span>Currency</span>
-        <span className='flex text-gray-300 space-x-2 items-center'>
+        <span className='flex text-gray-400 space-x-2 items-center text-sm'>
         <span>NGN</span>
         <ArrowForwardOutlined/>
         </span>
@@ -31,7 +31,7 @@ const page = () => {
         <span >
         Notifications
         </span>
-        <span>
+        <span className='text-gray-400'>
             <ArrowForwardOutlined fontSize='medium'/>
         </span>
         </span>
