2ce1dc42f4b2526703a616c5efca545fa5e28ad1
Mathew <111003952+mathoks@users.noreply.github.com>
2024-06-26 11:00:56 +0100

    Update Nav.1jsx

--- app/ui/Nav.jsx
+++ app/ui/Nav.jsx
@@ -70,8 +70,8 @@ useEffect(()=>{
       </Link>
       </section>
       <section>
-      <div className="flex justify-center shadow-my">
-        <div className="border flex items-center justify-between bg-white p-2 shadow-my">
+      <div className="flex justify-center">
+        <div className=" flex items-center justify-between bg-white p-2 shadow-my">
         <SearchOutlined className=" text-[#6A0DAD]" fontSize="medium" sx={{ zIndex: 80}}/>
         <input placeholder="find here" className="placeholder:text-center text-zinc-950  pl-4 pr-4 focus:outline-none w-[98%]"/>
         <LocationOn onClick={DrawerHandler} className="text-[#6A0DAD] sm:pr-2" fontSize="medium" />
