d4c3f3e06556f570e62a5a02f679d64d0ed22b87
Mathew <111003952+mathoks@users.noreply.github.com>
2024-07-02 18:29:40 +0100

    Update countries.jsgh3

--- app/lib/utills/countries.js
+++ app/lib/utills/countries.js
@@ -257,7 +257,7 @@ import { CancelOutlined } from '@mui/icons-material';
        
        <ul id='loc' className='flex space-x-2 mb-8 overflow-x-scroll text-[12px] max-w-[99%] pl-1 text-nowrap m-1  text_shadow2' ></ul>
        
-        <select onChange={handle} onBeforeInput={()=>console.log(';;;')}   id='location' name='location' enterKeyHint = 'done' required  className=' p-4 shadow flex w-[97%]'>
+        <select onInput={handle} onBeforeInput={()=>console.log(';;;')}   id='location' name='location' enterKeyHint = 'done' required  className=' p-4 shadow flex w-[97%]'>
         {<option disabled value= '' selected >{message}</option>}
       { con?.map((obj, id)=> <option   value={ obj.hasOwnProperty('name') ?  obj.name : obj.hasOwnProperty('market')? obj.market : obj} data-location ={ obj.hasOwnProperty('name') ? 'state' : obj.hasOwnProperty('market') ? 'market' : 'country'} key={id}  className='w-fit text-sm'>
                
