a2164b741669166995aad24648169a29ea5c9ace
mathoks <mathoks2012@gmail.com>
2024-07-02 08:47:01 +0100

    fixsettings39

--- app/lib/utills/countries.js
+++ app/lib/utills/countries.js
@@ -217,7 +217,7 @@ import { CancelOutlined } from '@mui/icons-material';
       <section className='flex flex-col justify-start  space-y-4  w-[99%] overflow-y-clip m-1'>
        <label>choose location</label>
        <ul id='loc' className='flex space-x-2 mb-8 overflow-x-scroll max-w-[99%] pl-1 text-nowrap m-1' onClick={handledelete}></ul>
-        <select onInput={handle}  multiple size={1} id='location' name='location' enterKeyHint = 'done' required  className=' p-4 shadow flex w-[97%]'>
+        <select onInput={handle} data-native-menu = "false" multiple size={3} id='location' name='location' enterKeyHint = 'done' required  className=' p-4 shadow flex w-[97%]'>
        <option  disabled>select an option</option>
       { con.map((obj, id)=> <option   value={ obj.hasOwnProperty('name') ?  obj.name : obj.hasOwnProperty('market')? obj.market : obj} data-location ={ obj.hasOwnProperty('name') ? 'state' : obj.hasOwnProperty('market') ? 'market' : 'country'} key={id} className='w-fit text-sm'>
                 {obj.hasOwnProperty('name') ?  obj.name : obj.hasOwnProperty('market') ? obj.market : obj}
