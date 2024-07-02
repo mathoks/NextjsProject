8bf9508c39765c97559e3bdf24dfcb2a31de7b6e
Mathew <111003952+mathoks@users.noreply.github.com>
2024-07-02 17:22:39 +0100

    Update countriegys.js

--- app/lib/utills/countries.js
+++ app/lib/utills/countries.js
@@ -245,11 +245,9 @@ import { CancelOutlined } from '@mui/icons-material';
   return () => {
     locationSelect?.removeEventListener('change', handleLocationChange);
   };
-}, [val]);
+}, [val])
 
-    useEffect (()=>
-      setcont(locations.map(({ country }) => country));
-    ,[])
+    useEffect (()=> setcont(locations.map(({ country }) => country)) ,[])
     return (
         <div className='flex flex-col space-y-4 mx-auto w-[99%]'>
         
