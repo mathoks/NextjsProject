c6be2f3082b554bd3413e95f7cf28541e2acdf6f
mathoks <mathoks2012@gmail.com>
2024-07-14 22:32:55 +0100

    updates10

--- app/ui/uiForms/branchForm.jsx
+++ app/ui/uiForms/branchForm.jsx
@@ -5,22 +5,23 @@ import React from 'react'
 const BranchForm = () => {
   return (
     <div className='mx-auto flex flex-col space-y-2'>
-      <form className='flex flex-col space-y-2 w-60 ring-2 p-6 rounded-md shadow-md'>
+      <form className='flex flex-col space-y-2 w-80 ring-2 ring-[#6A0DAD] p-6 rounded-md shadow-md'>
       <section className='flex flex-col space-y-1'>
       <label htmlFor='name'>Name</label>
-      <input />
+      <input className='p-2.5'/>
       </section>
         <section className='flex flex-col space-y-1'>
             <label htmlFor='address'>Address</label>
-            <input/>
+            <input  className='p-2.5'/>
         </section>
         <section className=''>
             <Countries/>
         </section>
-        <section className='flex flex-col space-y-1'>
+        <section className='flex flex-col space-y-1 pb-2'>
             <label htmlFor='phone'> phone number</label>
-            <input/>
+            <input  className='p-2.5'/>
         </section>
+        <button className='rounded-md bg-[#6A0DAD] py-2.5  text-white'>Submit</button>
       </form>
     </div>
   )
