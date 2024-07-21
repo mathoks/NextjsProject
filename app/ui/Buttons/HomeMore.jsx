c92c8cba6f09a03ec54a580c3efcde4df540e216
mathoks <mathoks2012@gmail.com>
2024-07-21 07:44:45 +0100

    updates30

--- app/ui/Buttons/HomeMore.jsx
+++ app/ui/Buttons/HomeMore.jsx
@@ -19,7 +19,7 @@ const HomeMore = ({id}) => {
 
   return (
     <button onClick={handleOpen} data-fab={id} key={id} aria-label="modal" >
-        <MoreVertOutlined  data-fab={id} sx={{color: '#6A0DAD'}}/>  
+        <MoreVertOutlined className="text-slate-300"  data-fab={id} />  
     </button>
   );
 };
