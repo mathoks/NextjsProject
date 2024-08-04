db7cb832ea0679506b67bec674999f17738428be
mathoks <mathoks2012@gmail.com>
2024-08-05 00:48:57 +0100

    updates353

--- app/ui/Tab.jsx
+++ app/ui/Tab.jsx
@@ -100,7 +100,7 @@ const Tab = () => {
                 id={id}
                 clickable
               /> */}
-              <div className={`flex text-sm space-x-1 ring-1 ring-white items-center text-nowrap rounded-full px-1.5 py-0.5  ${index === val? 'text-white  bg-[#6A0DAD] tab_text' : 'text-slate-600 bg-white'}`}>
+              <div className={`flex min-h-fit text-sm space-x-1 ring-1 ring-white items-center text-nowrap rounded-full px-1.5 py-0.5  ${index === val? 'text-white  bg-[#6A0DAD] tab_text' : 'text-slate-600 bg-white'}`}>
                 <span>{icon}</span>
                 <span className="text-nowrap shrink-0">{val}</span>
               </div>
