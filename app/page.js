1ed8e9b492579c02ef39b246133bbd93a35ee14f
mathoks <mathoks2012@gmail.com>
2024-08-24 10:08:00 +0100

    updates367

--- app/page.js
+++ app/page.js
@@ -1,14 +1,4 @@
 import "@/app/globals.css";
-import gsap from "gsap";
-import { ScrollTrigger } from "gsap/ScrollTrigger"
-
-gsap.registerPlugin(ScrollTrigger);
-gsap.to('progress', {
-  value: 100,
-  ease: 'none',
-  scrollTrigger: {scrub: 0.3}
-})
-
 export default async function Home() {
   return null;
 }
