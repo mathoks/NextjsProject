c3c043f435117c88e4e20fdc5a7bf6727de4dfed
mathoks <mathoks2012@gmail.com>
2024-08-25 10:06:55 +0100

    updates370

--- app/ui/uiForms/WriteReview.jsx
+++ app/ui/uiForms/WriteReview.jsx
@@ -1,29 +1,67 @@
-'use client'
-import { useAppSelector } from '@/app/lib/hooks/hooks'
-import React from 'react'
-import { connect } from 'react-redux'
+"use client";
+import { useAppSelector } from "@/app/lib/hooks/hooks";
+import React from "react";
+import { useEffect, useState } from "react";
+import { connect } from "react-redux";
 
 const WriteReview = () => {
-    const ReviewState = useAppSelector((state)=>state.review.showBox)
-    console.log(ReviewState)
+  const ReviewState = useAppSelector((state) => state.review.showBox);
+  const [active, setActive] = useState(true);
+  const handlechange = (e) => {
+    const { value } = e.target;
+    if (value.length > 2) {
+      setActive(false);
+      e.target.nextSibling.style.display = "block";
+      e.target.nextSibling.innerText = `${value.length}/200`;
+      if (value.length > 200) {
+        e.target.nextSibling.style.color = "red";
+        setActive(true);
+      } else {
+        e.target.nextSibling.style.color = "black";
+      }
+    } else {
+      setActive(true);
+      e.target.nextSibling.style.display = "hidden";
+      e.target.nextSibling.innerText = "";
+    }
+  };
+
+  useEffect(() => {});
+
   return (
-    <div className={` bg-slate-200 grid place-items-center rounded-sm items-center p-2 m-4 ${ReviewState ? 'visible' : 'invisible h-0'}`}>
-    <form className='space-y-2 mx-auto'>
-      <div className='w-full'>
-      <textarea  rows={4} cols={38} autoFocus  className=' bg-white rounded-t-sm w-full p-2'/>
-      </div>
-     <span className='flex justify-end'>
-      <button className='ring-1 px-2.5 py-1 text-blue-400 rounded-full'>submit</button>
-      </span>
+    <div
+      className={` bg-slate-200 grid place-items-center rounded-sm items-center p-2 m-4 ${
+        ReviewState ? "visible" : "invisible h-0"
+      }`}
+    >
+      <form className="space-y-2 mx-auto" action={() => console.log("jj")}>
+        <textarea
+          minLength={2}
+          maxLength={200}
+          required
+          id="text-rev"
+          onChange={handlechange}
+          rows={4}
+          cols={38}
+          autoFocus
+          className=" bg-white rounded-t-sm  p-2"
+        />
+        <pre className="text-[12px]"></pre>
+        <span className="flex justify-end">
+          <button
+            disabled={active}
+            className="ring-1 px-2.5 py-1 text-blue-400 rounded-full disabled:opacity-50 shadow-md"
+          >
+            submit
+          </button>
+        </span>
       </form>
     </div>
-  )
-}
+  );
+};
 
-const mapStateToProps = (state)=>({
-    review: state.review.showBox
-  })
- 
-  
-  export default connect(mapStateToProps)(WriteReview)
+const mapStateToProps = (state) => ({
+  review: state.review.showBox,
+});
 
+export default connect(mapStateToProps)(WriteReview);
